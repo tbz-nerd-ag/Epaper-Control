ef64e5d61b8f29f5776becae8322c63b763df0b6
Noora Liv <j.czinzoll@ingressy.de>
2026-05-09 16:20:45 +0200

    Update main.go

--- main.go
+++ main.go
@@ -5,9 +5,8 @@ import (
 )
 
 func main() {
-	//r := gin.Default()
-
-	//r.Run("0.0.0.0:70")
-	untis.Get_room_from_json()
+	untis.Schedule()
+	//mqtt.ConnecttoMQTT()
+	//untis.Get_room_from_json()
 
 }
