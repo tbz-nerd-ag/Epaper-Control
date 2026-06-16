cf5ba7d512446d6f3980a831cb1499ef05bec26b
ingressy <j.czinzoll@ingressy.de>
2026-06-16 11:51:38 +0200

    types fix

--- rest/types.go
+++ rest/types.go
@@ -1,9 +1,27 @@
+package rest
+
 // ErrorResponse
 type ErrorResponse struct {
-	Error string `json:"error"`
+	Error string `json:"error" example:"Ungültiger Token"`
 }
 
 // DeviceResponse
 type DeviceResponse struct {
 	Device string `json:"device"`
+}
+
+type badrequest struct {
+	Error string `json:"error" example:"Ungültige Anfrage"`
+}
+
+type WartungResponse struct {
+	Wartung bool `json:"wartung" example:"false"`
+}
+
+type FileNameResponse struct {
+	FileName string `json:"log_filename" example:"/root/logs/log.log"`
+}
+
+type WartungSleepResponse struct {
+	WartungSleepTime int `json:"wartung_sleep_time" example:"20"`
 }
